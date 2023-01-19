f4c4b7dd0316d372d534308b6157d56da6fadbc5
Thanu <Thanu@DESKTOP-DFAO42P>
2023-01-19 16:16:04 +0530

    hi

--- src/test/java/Maven_Practice/Thanu1test.java
+++ src/test/java/Maven_Practice/Thanu1test.java
@@ -3,6 +3,10 @@ package Maven_Practice;
 import org.testng.annotations.Test;
 
 public class Thanu1test {
+public static class git {
+
+	}
+
 @Test
 public void sample1() {
 	System.out.println("hai");
